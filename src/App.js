6d370ad020f2bc13da8f29f66bf56a4512b3553a
ArmanAru <armanhosen768@gmail.com>
2022-10-11 09:16:36 +0600

    createComponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const router = createBrowserRouter([
+
+  ])
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Componenet/Statistics/Statistics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Statistics = () => {
+    return (
+        <div>
+            <h3>This is statistics Chart</h3>
+        </div>
+    );
+};
+
+export default Statistics;
